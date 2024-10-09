39626a1d757042868b4c6c3bc2c54ba799dfbf25
MysticalFire <42976120+MysticalFire@users.noreply.github.com>
2024-10-09 18:38:35 -0400

    rotation fix and shooting works

--- Assets/Scripts/Player/Pawn/PawnNetworkController.cs
+++ Assets/Scripts/Player/Pawn/PawnNetworkController.cs
@@ -11,26 +11,35 @@ public class PawnNetworkController : NetworkBehaviour
     bool isAlive = true;
     [SerializeField] float movementSpeed = 4f;
     [SerializeField] float gamepadDeadzone = 0.1f;
-    //[SerializeField] RigidBody myRB;
-
+    [Header("Shoot Settings")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawnPoint;
+    [SerializeField] float fireRate = 0.1f;
+    [Space(10)]
     Vector2 moveDir, mouseDir;
 
     bool isUsingController = false;
-
+    [SerializeField] private string currentControlScheme;
+    public PlayerInput playerInput;
 
     private void Update()
     {
-        if (!IsOwner) return;
+        //if (!IsOwner) return;
 
         MovePlayer();
         LookAtAngle();
 
+        if (playerInput.currentControlScheme != currentControlScheme)
+        {
+            OnControlSchemeChanged();
+            currentControlScheme = playerInput.currentControlScheme;
+        }
     }
 
 
     void OnLook(InputValue value)
     {
-        if (!IsOwner) return;
+        //if (!IsOwner) return;
         if (!isAlive) return;
 
         mouseDir = value.Get<Vector2>();
@@ -38,16 +47,35 @@ public class PawnNetworkController : NetworkBehaviour
 
     void OnMove(InputValue value)
     {
-        if (!IsOwner) return;
+        //if (!IsOwner) return;
         if (!isAlive) return;
 
         moveDir = value.Get<Vector2>() * -1;
     }
 
-    void OnDeviceChange(PlayerInput p)
+    void OnFire(InputValue value)
     {
-        if (!IsOwner) return;
-        isUsingController = p.currentControlScheme.Equals("Gamepad") ? true : false;
+        //if (!IsOwner) return;
+        if (!isAlive) return;
+
+        if (value.isPressed && Time.time > fireRate)
+        {
+            Fire();
+            fireRate = Time.time + fireRate;
+            Debug.Log("Fire" + fireRate + " " + Time.time);
+        }
+    }
+
+    public void OnControlSchemeChanged()
+    {
+        if (playerInput.currentControlScheme == "Keyboard&Mouse")
+        {
+            Debug.Log("Keyboard & Mouse");
+        }
+        else
+        {
+            Debug.Log("Gamepad");
+        }
     }
 
     void MovePlayer()
@@ -57,14 +85,14 @@ public class PawnNetworkController : NetworkBehaviour
 
     void LookAtAngle()
     {
-        if(isUsingController)
+        if (currentControlScheme == "Gamepad")
         {
-            if(Mathf.Abs(mouseDir.x) > gamepadDeadzone || Mathf.Abs(mouseDir.y) > gamepadDeadzone)
+            if (Mathf.Abs(mouseDir.x) > gamepadDeadzone || Mathf.Abs(mouseDir.y) > gamepadDeadzone)
             {
                 Vector3 lookDir = Vector3.right * mouseDir.x + Vector3.forward * mouseDir.y;
-                if(lookDir.sqrMagnitude > 0f)
+                if (lookDir.sqrMagnitude > 0f)
                 {
-                    Quaternion newRot = Quaternion.LookRotation(lookDir, Vector3.up);
+                    Quaternion newRot = Quaternion.Euler(Quaternion.LookRotation(lookDir, Vector3.up).eulerAngles + new Vector3(0, -90, 0));
                     gunPivot.transform.rotation = Quaternion.RotateTowards(gunPivot.transform.rotation, newRot, Time.deltaTime * 1000);
                 }
             }
@@ -75,11 +103,18 @@ public class PawnNetworkController : NetworkBehaviour
             Plane testPlane = new Plane(Vector3.up, Vector3.zero);
             float rayDist;
 
-            if(testPlane.Raycast(ray, out rayDist))
+            if (testPlane.Raycast(ray, out rayDist))
             {
                 Vector3 point = ray.GetPoint(rayDist);
                 gunPivot.transform.LookAt(new Vector3(point.x, gunPivot.transform.position.y, point.z));
             }
         }
     }
+
+    void Fire()
+    {
+        NetworkObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, gunPivot.transform.rotation).GetComponent<NetworkObject>();
+        bullet.Spawn();
+    }
+
 }
